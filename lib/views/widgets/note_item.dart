ec5f215559de3345d0ef302aa5cdf203b5ced836
Kerolos Farah <k.farah2124@gmail.com>
2024-12-17 13:42:55 +0200

    create edit note view

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,65 +1,77 @@
 import 'package:flutter/material.dart';
 
+import '../edit_note_view.dart';
+
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: const Color(0xffFFCC80),
-      child: Padding(
-        padding: const EdgeInsetsDirectional.only(
-          top: 24,
-          bottom: 24,
-          start: 24,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              contentPadding: const EdgeInsets.all(0),
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsetsDirectional.only(
-                  top: 16,
-                  bottom: 16,
-                  end: 16,
-                ),
-                child: Text(
-                  'Build your career with tharwat samy',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const EditNoteView(),
+          ),
+        );
+      },
+      child: Card(
+        color: const Color(0xffFFCC80),
+        child: Padding(
+          padding: const EdgeInsetsDirectional.only(
+            top: 24,
+            bottom: 24,
+            start: 24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                contentPadding: const EdgeInsets.all(0),
+                title: const Text(
+                  'Flutter Tips',
                   style: TextStyle(
-                    color: Colors.black.withValues(alpha: 0.5),
-                    fontSize: 18,
+                    color: Colors.black,
+                    fontSize: 26,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsetsDirectional.only(
+                    top: 16,
+                    bottom: 16,
+                    end: 16,
+                  ),
+                  child: Text(
+                    'Build your career with tharwat samy',
+                    style: TextStyle(
+                      color: Colors.black.withValues(alpha: 0.5),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                  padding: const EdgeInsetsDirectional.only(end: 16),
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 24,
                   ),
                 ),
               ),
-              trailing: IconButton(
-                padding: const EdgeInsetsDirectional.only(end: 16),
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 24,
+              Padding(
+                padding: const EdgeInsetsDirectional.only(end: 24),
+                child: Text(
+                  'May 21, 2022',
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: 0.4),
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(end: 24),
-              child: Text(
-                'May 21, 2022',
-                style: TextStyle(
-                  color: Colors.black.withValues(alpha: 0.4),
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
