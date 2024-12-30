7d14bcf2dc14636c32bd91cffb240af2ec9e9d22
Kerolos Farah <k.farah2124@gmail.com>
2024-12-30 12:06:20 +0200

    format date time

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../../constants.dart';
 import '../../cubits/add_note_cubit/add_note_cubit.dart';
@@ -81,7 +82,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
                       Note(
                         title: titleController.text,
                         subTitle: contentController.text,
-                        date: DateTime.now().toString(),
+                        date: DateFormat('dd-mm-yyyy').format(DateTime.now()),
                         color: Colors.blue.r,
                       ),
                     );
